48f2d1798d3314afb2026e2ab08ead61048a0896
Pratap <pratapkc6@gmail.com>
2021-03-13 22:07:39 +0545

    initital commit

---
+++ src/main/java/com/subidha/Employee.java
@@ -0,0 +1,32 @@
+package com.subidha;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+class Employee {  
+
+@Id
+private int id; 
+private String firstName,lastName;  
+  
+public int getId() {  
+    return id;  
+}  
+public void setId(int id) {  
+    this.id = id;  
+}  
+public String getFirstName() {  
+    return firstName;  
+}  
+public void setFirstName(String firstName) {  
+    this.firstName = firstName;  
+}  
+public String getLastName() {  
+    return lastName;  
+}  
+public void setLastName(String lastName) {  
+    this.lastName = lastName;  
+}  
+  
+}
---
+++ src/test/java/com/subidha/HibernateCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subidha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
